d9beacbe669f2425eda281b7a63cc4dea98465cd
Aura <103008984+aurachl@users.noreply.github.com>
2022-06-15 16:42:14 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { v4 as uuidv4 } from 'uuid'; //Add to "fix" warning
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
